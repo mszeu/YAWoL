25b93c690ddedb53efd218b9706505f94b94c1e6
Marco Simone Zuppone <m.zuppone@computer.org>
2022-06-08 17:58:10 +0100

    added interval and times parameter

--- wakeup.py
+++ wakeup.py
@@ -5,6 +5,7 @@
 
 import argparse
 import socket
+import time
 
 
 def wol(lunaMacAddress: bytes, port: int):
@@ -27,13 +28,26 @@ if __name__ == '__main__':
                "author at msz@msz.eu")
     parser.add_argument("MACAddress", type=str,
                         help="The MAC Address of the Ethernet device to wake. Example: 0011223344AA")
-    parser.add_argument("--port", type=int, default=7, help="The port where to send the magic packet")
+    parser.add_argument("--port", type=int, default=7, help="The port where to send the magic packet",
+                        choices=range(0, 65535), )
+    parser.add_argument("--times", type=int, default=1, help="times to send the magic packet")
+    parser.add_argument("--interval", type=int, default=0,
+                        help="interval, in seconds, between packets")
     args = parser.parse_args()
     chars = set('0123456789abcdedABCDEF')
     if len(args.MACAddress) != 12 and not (all((c in chars) for c in args.MACAddress)):
         print("the MAC Address needs to be hexadecimal without spaces or signs between one byte and the other and the "
               "bytes need to be 6")
         exit()
+    if args.times < 1:
+        args.times = 1
+    if args.interval < 0:
+        args.interval = 0
+    if args.port < 0 or args.port > 65535:
+        print('The port need to be between 0 and 65535')
+        exit()
     # pass to wol the mac address of the ethernet port of the appliance to wakeup
-    wol(bytearray.fromhex(args.MACAddress), args.port)
-
+    for x in range(args.times):
+        wol(bytearray.fromhex(args.MACAddress), args.port)
+        if x + 1 != args.times:
+            time.sleep(args.interval)
