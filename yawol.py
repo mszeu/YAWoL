e0d691b36e57c1f71e21743f6e459b485962b3f8
Marco Simone Zuppone <m.zuppone@computer.org>
2024-08-05 16:02:34 +0100

    added support for mac addresses with : or - chars

--- yawol.py
+++ yawol.py
@@ -6,7 +6,8 @@
 import argparse
 import socket
 import time
-from sys import exit
+import sys
+
 
 def wol(lunaMacAddress: bytes, port: int):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,13 +21,17 @@ def wol(lunaMacAddress: bytes, port: int):
 
 
 if __name__ == '__main__':
-    VERSION = "1.1.1"
+    VERSION = "1.2"
+    REPLACEMENTS = [
+        ("-", ""),
+        (":", "")]
+
     print("YAWoL - by Marco S. Zuppone - msz@msz.eu - https://msz.eu - version", VERSION)
     print("Program licensed under GNU AFFERO GENERAL PUBLIC LICENSE version 3")
     print("")
     parser = argparse.ArgumentParser(
         description="Sends the WoL magic packet to the specified MAC address",
-        epilog="For any questions, feedback, suggestions, send money (yes...it's a dream I know) you can contact the "
+        epilog="For any questions, feedback, suggestions, donations (I'm a dreamer, I know) you can contact the "
                "author at msz@msz.eu")
     parser.add_argument("MACAddress", type=str,
                         help="The MAC Address of the Ethernet device to wake. Example: 0011223344AA")
@@ -37,17 +42,19 @@ if __name__ == '__main__':
                         help="Interval, in seconds, between packets")
     args = parser.parse_args()
     chars = set('0123456789abcdefABCDEF')
+    for old, new in REPLACEMENTS:
+        args.MACAddress.replace(old, new)
     if len(args.MACAddress) != 12 and not (all((c in chars) for c in args.MACAddress)):
         print("the MAC Address needs to be hexadecimal without spaces or signs between one byte and the other and the "
               "bytes need to be 6")
-        exit()
+        sys.exit()
     if args.times < 1:
         args.times = 1
     if args.interval < 0:
         args.interval = 0
     if args.port < 0 or args.port > 65535:
-        print('The port need to be between 0 and 65535')
-        exit()
+        print('The port needs to be between 0 and 65535')
+        sys.exit()
     # pass to wol the mac address of the ethernet port of the appliance to wakeup
     for x in range(args.times):
         wol(bytearray.fromhex(args.MACAddress), args.port)
